7f766bcdcf8039bc1dc9a772cbf0b207b476467e
Muhammad Noman <muhammad.noman@laksol.com>
2023-10-12 05:37:38 +0500

    Task 4

---
+++ database/migrations/2023_10_09_141357_create_people_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('people', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->unsignedBigInteger('manager_id');
+            $table->unsignedBigInteger('employee_id');
+            $table->foreign('manager_id')->references('id')->on('managers');
+            $table->foreign('employee_id')->references('id')->on('employees');
+            $table->timestamps();
+
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('people');
+    }
+};
